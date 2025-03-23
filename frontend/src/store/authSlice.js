508a4eedfcd27e06f6568fc63246acaf23e93112
Masoud Barzegar <masood.barzegar@gmail.com>
2025-03-24 10:47:05 +1100

    feat: Add loading and error states to authSlice.js - Added `loading` and `error` states to track authentication progress and errors. - Updated `loginSuccess`, `logoutSuccess`, and added `authError` and `setLoading` reducers.

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -2,23 +2,34 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
 	user: null,
-	isAuthenticated : false,
+	isAuthenticated: false,
+	loading: true,
+	error: null,
 };
 
 const authSlice = createSlice({
 	name: 'auth',
 	initialState,
 	reducers: {
-		loginSuccess(state, action) {
+		loginSuccess: (state, action) => {
 			state.user = action.payload;
 			state.isAuthenticated = true;
+			state.loading = false;
 		},
-		logout(state) {
+		logoutSuccess: (state) => {
 			state.user = null;
-			state.isAuthenticated = false
+			state.isAuthenticated = false;
+			state.loading = false;
+		},
+		authError: (state, action) => {
+			state.error = action.payload;
+			state.loading = false;
+		},
+		setLoading: (state, action) => {
+			state.loading = action.payload;
 		},
 	},
 });
 
-export const { loginSuccess, logout } = authSlice.actions;
+export const { loginSuccess, logoutSuccess, authError, setLoading } = authSlice.actions;
 export default authSlice.reducer;
